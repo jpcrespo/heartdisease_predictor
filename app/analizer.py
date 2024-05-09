a095b3574a3c3efa32f178cdc6e38a4ac4b89e30
Juan.py <jp.crespo.vargas@gmail.com>
2024-05-09 11:22:24 +0300

    decisión tree

--- app/analizer.py
+++ app/analizer.py
@@ -1,5 +1,8 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import accuracy_score, f1_score,recall_score,precision_score, ConfusionMatrixDisplay
 
 
 #Using Pearson Correlation
@@ -9,4 +12,22 @@ def P_corr(X):
     sns.heatmap(cor, annot=True, cmap=plt.cm.Accent)
     plt.title("Pearson Correlation",fontsize=30)
     plt.savefig('output/Pearson_corr.png', dpi=300)
+
+def splitdata(X,y):
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3)  
+    return X_train, X_test, y_train, y_test
+
+
+def decision_tree(X_train,y_train,X_test,y_test):
+    DT = tree.DecisionTreeClassifier(max_depth = 40)
+    DT.fit(X_train,y_train)
+    aucc = DT.score(X_train, y_train)
+    y_pred = DT.predict(X_test)
+    acc = accuracy_score(y_test,y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test,y_pred)
+    ConfusionMatrixDisplay.from_estimator(DT, X_test, y_test)
+    plt.title('Confusion Matrix\nDecision Tree',fontsize=18)
+    plt.savefig('output/DT.png', dpi=300)
  
--- solution.py
+++ solution.py
@@ -1,6 +1,9 @@
 from app.data import data_info
 from app.data_prepross import preprocess
-from app.analizer import P_corr
+from app.analizer import P_corr, splitdata, decision_tree
+
 dataset = data_info()
 X,y = preprocess(dataset)
 P_corr(X)
+X_train, X_test, y_train, y_test = splitdata(X,y)
+decision_tree(X_train,y_train,X_test,y_test)
