f748f630354eff87b46fb5a15182f88424f7bf91
Juan.py <jp.crespo.vargas@gmail.com>
2024-05-11 09:12:36 +0300

    benchmark

--- app/analizer.py
+++ app/analizer.py
@@ -5,6 +5,8 @@ from ML and anothers to show results
 
 
 import seaborn as sns
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier 
@@ -49,96 +51,155 @@ def decision_tree(X_train,y_train,X_test,y_test):
     ConfusionMatrixDisplay.from_estimator(DT, X_test, y_test)
     plt.title('Confusion Matrix\nDecision Tree',fontsize=18)
     plt.savefig('output/DT.png', dpi=300)
- 
+    return acc,prec,rec,f1
 
 
 def random_forest(X_train, y_train, X_test, y_test):
     RF = RandomForestClassifier(n_estimators=100, max_depth=40, random_state=42)
     model = RF.fit(X_train, y_train)
-    train_accuracy = model.score(X_train, y_train)
+    aucc = model.score(X_train, y_train)
     y_pred = RF.predict(X_test)
-    test_accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
     with open('output/random_forest.txt', 'w') as f:
         f.write("*******************************************************************\n")
         f.write("The Scores for Random Forest algorithm\n")
-        f.write(f'\nPrecision training: {train_accuracy:.4f}')
-        f.write(f'\nAccuracy testing: {test_accuracy:.4f}')
-        f.write(f'\nPrecision testing: {precision:.4f}')
-        f.write(f'\nRecall training: {recall:.4f}')
+        f.write(f'\nPrecision training: {aucc:.4f}')
+        f.write(f'\nAccuracy testing: {acc:.4f}')
+        f.write(f'\nPrecision testing: {prec:.4f}')
+        f.write(f'\nRecall training: {rec:.4f}')
         f.write(f'\nF1 training: {f1:.4f}')
     ConfusionMatrixDisplay.from_estimator(RF, X_test, y_test)
     plt.title('Confusion Matrix\nRandom Forest',fontsize=18)
     plt.savefig('output/RF.png', dpi=300)
-
+    return acc,prec,rec,f1
 
 def suppor_vector_machine(X_train, y_train, X_test, y_test):
     svc = SVC(gamma='auto')
     model = svc.fit(X_train, y_train)
-    train_accuracy = model.score(X_train, y_train)
+    aucc = model.score(X_train, y_train)
     y_pred = svc.predict(X_test)
-    test_accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
     with open('output/suppor_vector_machine.txt', 'w') as f:
         f.write("*******************************************************************\n")
         f.write("The Scores for Random Forest algorithm\n")
-        f.write(f'\nPrecision training: {train_accuracy:.4f}')
-        f.write(f'\nAccuracy testing: {test_accuracy:.4f}')
-        f.write(f'\nPrecision testing: {precision:.4f}')
-        f.write(f'\nRecall training: {recall:.4f}')
+        f.write(f'\nPrecision training: {aucc:.4f}')
+        f.write(f'\nAccuracy testing: {acc:.4f}')
+        f.write(f'\nPrecision testing: {prec:.4f}')
+        f.write(f'\nRecall training: {rec:.4f}')
         f.write(f'\nF1 training: {f1:.4f}')
     ConfusionMatrixDisplay.from_estimator(svc, X_test, y_test)
     plt.title('Confusion Matrix\nSupport Vector Machine',fontsize=18)
     plt.savefig('output/SVC.png', dpi=300)
-
+    return acc,prec,rec,f1
 
 def naive_bayes(X_train, y_train, X_test, y_test):
     nb = GaussianNB()
     model = nb.fit(X_train, y_train)
-    train_accuracy = model.score(X_train, y_train)
+    aucc = model.score(X_train, y_train)
     y_pred = nb.predict(X_test)
-    test_accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
     with open('output/naive_bayes.txt', 'w') as f:
         f.write("*******************************************************************\n")
         f.write("The Scores for Random Forest algorithm\n")
-        f.write(f'\nPrecision training: {train_accuracy:.4f}')
-        f.write(f'\nAccuracy testing: {test_accuracy:.4f}')
-        f.write(f'\nPrecision testing: {precision:.4f}')
-        f.write(f'\nRecall training: {recall:.4f}')
+        f.write(f'\nPrecision training: {aucc:.4f}')
+        f.write(f'\nAccuracy testing: {acc:.4f}')
+        f.write(f'\nPrecision testing: {prec:.4f}')
+        f.write(f'\nRecall training: {rec:.4f}')
         f.write(f'\nF1 training: {f1:.4f}')
     ConfusionMatrixDisplay.from_estimator(nb, X_test, y_test)
     plt.title('Confusion Matrix\nNaive Bayes',fontsize=18)
     plt.savefig('output/NB.png', dpi=300)
-
+    return acc,prec,rec,f1
 
 
 def K_N_N(X_train, y_train, X_test, y_test):
     knn = KNeighborsClassifier(n_neighbors=3)
     model = knn.fit(X_train, y_train)
-    train_accuracy = model.score(X_train, y_train)
+    aucc = model.score(X_train, y_train)
     y_pred = knn.predict(X_test)
-    test_accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
     with open('output/KNN.txt', 'w') as f:
         f.write("*******************************************************************\n")
         f.write("The Scores for Random Forest algorithm\n")
-        f.write(f'\nPrecision training: {train_accuracy:.4f}')
-        f.write(f'\nAccuracy testing: {test_accuracy:.4f}')
-        f.write(f'\nPrecision testing: {precision:.4f}')
-        f.write(f'\nRecall training: {recall:.4f}')
+        f.write(f'\nPrecision training: {aucc:.4f}')
+        f.write(f'\nAccuracy testing: {acc:.4f}')
+        f.write(f'\nPrecision testing: {prec:.4f}')
+        f.write(f'\nRecall training: {rec:.4f}')
         f.write(f'\nF1 training: {f1:.4f}')
     ConfusionMatrixDisplay.from_estimator(knn, X_test, y_test)
     plt.title('Confusion Matrix\nKNN',fontsize=18)
     plt.savefig('output/knn.png', dpi=300)
-
-
+    return acc,prec,rec,f1
     
+
+def benchmarkbar():
+    # Creación del DataFrame
+    data = {
+        'Model': ['decision_tree', 'random_forest', 'naive_bayes', 'support_vector_machine', 'K_N_N'],
+        'Accuracy': [0.670330, 0.868132, 0.846154, 0.857143, 0.846154],
+        'Precision': [0.720930, 0.877551, 0.906977, 0.860000, 0.857143],
+        'Recall': [0.632653, 0.877551, 0.795918, 0.877551, 0.857143],
+        'F1 Score': [0.673913, 0.877551, 0.848726, 0.868687, 0.857143]
+    }
+
+    bench_data = pd.DataFrame(data)
+
+    # Transformar los datos para que se ajusten al formato adecuado para seaborn
+    melted_data = bench_data.melt(id_vars='Model', var_name='Metric', value_name='Score')
+
+    # Crear un gráfico de barras
+    plt.figure(figsize=(12, 8))  # Ajustar el tamaño para mejor visualización
+    barplot = sns.barplot(data=melted_data, x='Metric', y='Score', hue='Model', palette='viridis')
+
+    # Mejoras estéticas
+    plt.title('Benchmark', fontsize=16)  # Título del gráfico
+    plt.xlabel('Métrica', fontsize=14)  # Etiqueta del eje X
+    plt.ylabel('Score', fontsize=14)  # Etiqueta del eje Y
+    plt.ylim(0.6, 0.9)  # Ajustar los límites del eje Y para mejor enfoque
+
+    # Ajustar la leyenda
+    plt.legend(title='Modelo', fontsize=12, title_fontsize='13', loc='upper left', bbox_to_anchor=(1, 1))  # Posicionamiento de la leyenda
+
+    # Ajustar el layout para evitar recortes y superposiciones
+    plt.tight_layout()
+
+    # Mostrar el gráfico
+    plt.savefig('output/barbench.png', dpi=300)
+
+
+def benchmark(bench_data):
+    categories = list(bench_data)[1:]  # Esto excluye la primera columna que es 'Model'
+    N = len(categories)
+
+    angles = [n/float(N) * 2 * np.pi for n in range(N)]
+    angles += angles[:1] 
+    fig, ax = plt.subplots(figsize=(8, 8), subplot_kw={'polar': True})
+    plt.xticks(angles[:-1],categories, color='b',size=18)
+    ax.set_rlabel_position(270)
+
+    for i in range(bench_data.shape[0]):
+        values = bench_data.loc[i].drop('Model').values.flatten().tolist()
+        values += values[:1]  # Se completa el círculo
+        ax.plot(angles,np.log(values), linewidth=2, linestyle='--', label=bench_data['Model'][i])
+        ax.fill(angles,np.log(values), alpha=0.05)
+
+    for label, angle in zip(ax.get_xticklabels(), angles):
+        if label.get_text() == 'Accuracy' or label.get_text() == 'Recall' :
+            label.set_horizontalalignment('left' if angle < np.pi else 'right')
+            label.set_verticalalignment('bottom' if angle < np.pi/2 or angle > 3*np.pi/2 else 'top')
+
+    ax.set_yticklabels([])
+    plt.legend(loc='upper right', bbox_to_anchor=(0.15, 0.15),fontsize=16)
+    plt.savefig('output/bench.png', dpi=300)
--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
+import pandas as pd
 from app.data import data_info
 from app.data_prepross import preprocess
 from app.analizer import P_corr, splitdata,\
       decision_tree, random_forest,naive_bayes,\
-      suppor_vector_machine, K_N_N
+      suppor_vector_machine, K_N_N, benchmarkbar, benchmark
 
 dataset = data_info()
 X,y = preprocess(dataset)
@@ -10,6 +11,28 @@ P_corr(X)
 X_train, X_test, y_train, y_test = splitdata(X,y)
 
 models = [decision_tree,random_forest,naive_bayes,suppor_vector_machine,K_N_N]
+name_models = ["decision_tree","random_forest","naive_bayes","suppor_vector_machine","K_N_N"]
+
+accuracy=[]
+precision=[]
+recall=[]
+f1=[]
 
 for model in models:
-    model(X_train,y_train,X_test,y_test)
+      s,p,r,f = model(X_train,y_train,X_test,y_test)
+      accuracy.append(s)
+      precision.append(p)
+      recall.append(r)
+      f1.append(f)
+
+#Create de Benchmark data
+bench_data = pd.DataFrame({
+    'Model': name_models,
+    'Accuracy': accuracy,
+    'Precision': precision,
+    'Recall': recall,
+    'F1 Score': f1
+})
+
+benchmark(bench_data)
+benchmarkbar()
